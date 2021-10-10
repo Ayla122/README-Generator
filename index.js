4f45330db68fb13cbfbfa43227fa201faff927a7
Ayla122 <ayladd122@gmail.com>
2021-10-10 14:17:04 -0700

    add internal modules & utils file

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
 
+// Internal modules
+const api = require('./utils/api.js');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
 // Inquirer prompts for userResponses
 const questions = [
     {
